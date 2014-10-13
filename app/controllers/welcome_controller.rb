292c2fb4db18dcda10e038fc956efc71674b062a
jcain3389 <jcain3389@yahoo.com>
2014-10-12 22:59:15 -0400

    render keys as json from controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,19 @@
 class WelcomeController < ApplicationController
   def index
+
+  end
+
+  def get_keys
+    @auth_data = {
+      "consumerKey" => ENV["yelp_consumer_key"],
+      "consumerSecret" => ENV["yelp_consumer_secret"],
+      "accessToken" => ENV["yelp_access_token"],
+      "accessTokenSecret" => ENV["yelp_access_token_secret"],
+      "serviceProvider" => {
+        "signatureMethod" => ENV["yelp_signature_secret"]
+      }
+    }
+
+    render :json => @auth_data
   end
 end
